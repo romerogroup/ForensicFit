248c601700b68104bfa860664bfbe572c919bce9
Pedram Tavadze <petavazohi@mix.wvu.edu>
2021-06-16 16:16:50 -0400

    temp for testing

--- forensicfit/__init__.py
+++ forensicfit/__init__.py
@@ -2,7 +2,7 @@
 
 from . import utils
 from . import core
-from . import database
-from .script_process_dir import process_directory
-from .store_on_db import store_on_db
+# from . import database
+# from .script_process_dir import process_directory
+# from .store_on_db import store_on_db
 # from .from_excel import from_excel
