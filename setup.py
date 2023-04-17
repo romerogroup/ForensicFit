3df1dc9ffc1af511b3782460b552c05a850aff54
Pedram Tavadze <petavazohi@mix.wvu.edu>
2023-04-17 14:31:04 -0400

    generated the packaging

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
-from distutils.core import setup
-from setuptools import find_packages
-import json
-import pathlib
-import os
+from setuptools import find_packages, setup
 from forensicfit import __version__ as version
 
 with open("requirements.txt") as f:
     requirements = []
-    optionals = []
+    extras = {}
     reqs = f.read().splitlines()
+    optional = False
     for req in reqs:
-        if req.startswith("#"):
+        if len(req) == 0:
             continue
-        if req.startswith("-"):
-            optionals.append(req[1:])
+        if req.startswith("#"):
+            req = req.replace("#", "").strip()
+            if "optional" in req.lower():
+                section = req.lower().replace('optional', '').strip()
+                extras[section] = []
+                optional = True
+            else:
+                extras[section].append(req)
         else:
             requirements.append(req)
 
@@ -24,17 +27,11 @@ setup(
     author='Pedram Tavadze',
     author_email='petavazohi@mix.wvu.edu',
     url="https://github.com/romerogroup/ForensicFit",
-    download_url=data["download_url"],
     license="LICENSE.txt",
-    install_requires=[
-        "opencv-python",
-        "numpy>=1.20",
-        "scipy",
-        "matplotlib",
-    ],
-    data_files=[("", ["LICENSE.txt"])],
+    install_requires=requirements,
+    extras_require=extras,
     scripts=["scripts/create_metadata.py", 
              'scripts/preprocess_bin_based.py', 
              'scripts/store_on_db.py'],
-    packages=find_packages(exclude=[".ML-convlolutional_net", "docs"]),
+    packages=find_packages(exclude=["docs", 'sphinx']),
 )
