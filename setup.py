4dcb91389e7e635a363c4d9023f0bc8bb2fce6e3
petavazohi <petavazohi@mix.wvu.edu>
2023-04-17 12:03:50 -0400

    checkpoint for setup.py

--- setup.py
+++ setup.py
@@ -5,26 +5,25 @@ import pathlib
 import os
 from forensicfit import __version__ as version
 
-{
-  "name": "forensicfit",
-  "version": "1.00",
-  "description": "A Python library for forensic image comarision.",
-  "author": "Pedram Tavadze, Meghan Prusinowski, Zachary Andrews, Colton Diges, Tatiana Trejos, Aldo H Romero ",
-  "email": "petavazohi@mix.wvu.edu, mnp0006@mix.wvu.edu, tatiana.trejos@mail.wvu.edu, alromero@mail.wvu.edu, ",
-  "url": "https://github.com/romerogroup/ForensicFit",
-  "download_url": "https://github.com/romerogroup/ForensicFit",
-  "status": "development",
-  "copyright": "Copyright 2021",
-  "date": "Jun 16th, 2021"
-}
+with open("requirements.txt") as f:
+    requirements = []
+    optionals = []
+    reqs = f.read().splitlines()
+    for req in reqs:
+        if req.startswith("#"):
+            continue
+        if req.startswith("-"):
+            optionals.append(req[1:])
+        else:
+            requirements.append(req)
 
 setup(
     name="forensicfit",
     version=version,
-    description=data["description"],
-    author=data["author"],
-    author_email=data["email"],
-    url=data["url"],
+    description="A Python library for forensic image comparision.",
+    author='Pedram Tavadze',
+    author_email='petavazohi@mix.wvu.edu',
+    url="https://github.com/romerogroup/ForensicFit",
     download_url=data["download_url"],
     license="LICENSE.txt",
     install_requires=[
@@ -34,7 +33,6 @@ setup(
         "matplotlib",
     ],
     data_files=[("", ["LICENSE.txt"])],
-    package_data={"": ["setup.json"]},
     scripts=["scripts/create_metadata.py", 
              'scripts/preprocess_bin_based.py', 
              'scripts/store_on_db.py'],
